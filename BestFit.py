11023a9b7fc31dd5cff10649ef168dc3c14316fc
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2020-05-08 21:43:37 +0200

    error handeld

--- BestFit.py
+++ BestFit.py
@@ -1,4 +1,10 @@
 def BestFit(se, holes):
+    minHole = None
     for k in holes:
         if (k.size() >= se):
-            return k;
+            if(minHole == None):
+                minHole = k
+            elif(minHole.size()>k.size()):
+                minHole=k
+    return minHole
+    return minHole
