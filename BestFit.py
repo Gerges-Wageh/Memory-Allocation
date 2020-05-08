562d0c6bc4f83baddabfbee29c3dd12c9ab49574
AdhamNour <58791852+AdhamNour@users.noreply.github.com>
2020-05-08 21:36:03 +0200

    Add files via upload

--- BestFit.py
+++ BestFit.py
@@ -1,9 +1,4 @@
 def BestFit(se, holes):
-    minv = None
     for k in holes:
         if (k.size() >= se):
-            if (minv == None):
-                minv = k
-            elif (k.size() < minv.size()):
-                minv = k
-    return minv
+            return k;
