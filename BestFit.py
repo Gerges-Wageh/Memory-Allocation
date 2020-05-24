f20bb358b404d9d5fb88d86b8d7f84e1a847680d
DJ Man <49337662+DJ-Man-2099@users.noreply.github.com>
2020-05-24 05:02:48 +0200

    Delete BestFit.py

--- BestFit.py
+++
@@ -1,9 +0,0 @@
-def BestFit(se, holes):
-    minHole = None
-    for k in holes:
-        if (k.size() >= se):
-            if(minHole == None):
-                minHole = k
-            elif(minHole.size()>k.size()):
-                minHole=k
-    return minHole
